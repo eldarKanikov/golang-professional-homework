f6cdc885146971a1273d78b7ac3fd8df90bf540b
eldarkanikov <kanikov90@gmail.com>
2024-12-15 04:25:00 +0400

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,78 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+const (
+	EMPTY_INPUT_UNPACK_ERROR                 = "input string is empty"
+	TWO_OR_MORE_DIGITS_TOGETHER_UNPACK_ERROR = "two or more digits together"
+	FIRST_ELEMET_IS_DIGIT_UNPACK_ERROR       = "first element is digit"
+)
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	runes, err := validateAndReturnRunes(input)
+	if err != nil {
+		return "", err
+	}
+
+	var result strings.Builder
+	for i := 0; i < len(runes); i++ {
+		curr, next := readCurrNextElements(runes, i)
+		var partString string
+		if isNotNumber(curr) {
+			if isCoefficient(next) {
+				coeff, _ := strconv.Atoi(string(next))
+				partString = unpackPartString(string(curr), coeff)
+			} else {
+				partString = string(curr)
+			}
+		}
+		result.WriteString(partString)
+	}
+	return result.String(), nil
+}
+
+func validateAndReturnRunes(input string) ([]rune, error) {
+	if input == "" {
+		return nil, errors.New(EMPTY_INPUT_UNPACK_ERROR)
+	}
+	twoOrMoreDigitsTogetherRegexp := regexp.MustCompile(`\d{2,}`)
+
+	if twoOrMoreDigitsTogetherRegexp.MatchString(input) {
+		return nil, errors.New(TWO_OR_MORE_DIGITS_TOGETHER_UNPACK_ERROR)
+	}
+
+	runes := []rune(input)
+	if unicode.IsDigit(runes[0]) {
+		return nil, errors.New(FIRST_ELEMET_IS_DIGIT_UNPACK_ERROR)
+	}
+	return runes, nil
+}
+
+func readCurrNextElements(runes []rune, index int) (curr rune, next rune) {
+	curr = runes[index]
+	if index+1 < len(runes) {
+		next = runes[index+1]
+	}
+	return curr, next
+}
+
+func isNotNumber(r rune) bool {
+	return !unicode.IsNumber(r)
+}
+
+func isCoefficient(r rune) bool {
+	return isNumber(r)
+}
+
+func isNumber(r rune) bool {
+	return unicode.IsNumber(r)
+}
+
+func unpackPartString(partString string, count int) string {
+	return strings.Repeat(partString, count)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -14,8 +14,12 @@ func TestUnpack(t *testing.T) {
 	}{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "abc0d0e", expected: "abe"},
+		{input: "abc0d0eйц4ы5", expected: "abeйццццыыыыы"},
+		{input: "!2 3", expected: "!!   "},
+		{input: "日2本3", expected: "日日本本本"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,12 +38,13 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
-		})
-	}
+	_, err := Unpack("3abc")
+	require.Truef(t, errors.Is(err, errors.New(FIRST_ELEMET_IS_DIGIT_UNPACK_ERROR)), "actual error %q", err)
+
+	_, err = Unpack("aaa10b")
+	require.Truef(t, errors.Is(err, errors.New(TWO_OR_MORE_DIGITS_TOGETHER_UNPACK_ERROR)), "actual error %q", err)
+
+	_, err = Unpack("")
+	require.Truef(t, errors.Is(err, errors.New(TWO_OR_MORE_DIGITS_TOGETHER_UNPACK_ERROR)), "actual error %q", err)
+
 }
