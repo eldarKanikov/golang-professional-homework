0e61e6cafde036e3a94cbd8a4b73909f878c05be
eldarkanikov <kanikov90@gmail.com>
2024-12-18 18:56:05 +0400

    HW2 is completed: made shorter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"regexp"
 	"strconv"
 	"strings"
 	"unicode"
@@ -15,65 +14,36 @@ var (
 )
 
 func Unpack(input string) (string, error) {
-	runes, err := validateAndReturnRunes(input)
-	if err != nil {
-		return "", err
+	if input == "" {
+		return "", ErrEmptyInputUnpackError
+	}
+	runes := []rune(input)
+
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrFirstElementIsDigitUnpackError
 	}
 
 	var result strings.Builder
 	for i := 0; i < len(runes); i++ {
-		curr, next := readCurrNextElements(runes, i)
+		curr := runes[i]
+		var next rune
+		if i+1 < len(runes) {
+			next = runes[i+1]
+		}
 		var partString string
-		if isNotNumber(curr) {
-			if isCoefficient(next) {
+		if !unicode.IsNumber(curr) {
+			if unicode.IsNumber(next) {
 				coeff, _ := strconv.Atoi(string(next))
-				partString = unpackPartString(string(curr), coeff)
+				partString = strings.Repeat(string(curr), coeff)
 			} else {
 				partString = string(curr)
+			}
+		} else {
+			if unicode.IsNumber(next) {
+				return "", ErrTwoOrMoreDigitsTogetherUnpackError
 			}
 		}
 		result.WriteString(partString)
 	}
 	return result.String(), nil
-}
-
-func validateAndReturnRunes(input string) ([]rune, error) {
-	if input == "" {
-		return nil, ErrEmptyInputUnpackError
-	}
-	twoOrMoreDigitsTogetherRegexp := regexp.MustCompile(`\d{2,}`)
-
-	if twoOrMoreDigitsTogetherRegexp.MatchString(input) {
-		return nil, ErrTwoOrMoreDigitsTogetherUnpackError
-	}
-
-	runes := []rune(input)
-	if unicode.IsDigit(runes[0]) {
-		return nil, ErrFirstElementIsDigitUnpackError
-	}
-	return runes, nil
-}
-
-func readCurrNextElements(runes []rune, index int) (curr rune, next rune) {
-	curr = runes[index]
-	if index+1 < len(runes) {
-		next = runes[index+1]
-	}
-	return curr, next
-}
-
-func isNotNumber(r rune) bool {
-	return !unicode.IsNumber(r)
-}
-
-func isCoefficient(r rune) bool {
-	return isNumber(r)
-}
-
-func isNumber(r rune) bool {
-	return unicode.IsNumber(r)
-}
-
-func unpackPartString(partString string, count int) string {
-	return strings.Repeat(partString, count)
 }
