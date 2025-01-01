b0b28b1be24de44cd98a224fe98ebe74dda87ea1
ekanikov-dev <61244087+eldarKanikov@users.noreply.github.com>
2025-01-02 01:03:44 +0400

    HW2 is completed (#2)
    * HW2 is completed
    
    * HW2 is completed: fixed linter, tests
    
    * HW2 is completed: made shorter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrEmptyInputUnpackError              = errors.New("input string is empty")
+	ErrTwoOrMoreDigitsTogetherUnpackError = errors.New("two or more digits together")
+	ErrFirstElementIsDigitUnpackError     = errors.New("first element is digit")
+)
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", ErrEmptyInputUnpackError
+	}
+	runes := []rune(input)
+
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrFirstElementIsDigitUnpackError
+	}
+
+	var result strings.Builder
+	for i := 0; i < len(runes); i++ {
+		curr := runes[i]
+		var next rune
+		if i+1 < len(runes) {
+			next = runes[i+1]
+		}
+		var partString string
+		if !unicode.IsNumber(curr) {
+			if unicode.IsNumber(next) {
+				coeff, _ := strconv.Atoi(string(next))
+				partString = strings.Repeat(string(curr), coeff)
+			} else {
+				partString = string(curr)
+			}
+		} else {
+			if unicode.IsNumber(next) {
+				return "", ErrTwoOrMoreDigitsTogetherUnpackError
+			}
+		}
+		result.WriteString(partString)
+	}
+	return result.String(), nil
 }
