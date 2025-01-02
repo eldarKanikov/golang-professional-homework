7780f67497e13487bb62a2aea182a4aade36f425
eldarkanikov <kanikov90@gmail.com>
2025-01-02 22:18:01 +0400

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,17 +11,72 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
+	capacity    int
+	valuesQueue List
+	keysQueue   List // to store the consequence of keys
+	valuesItems map[Key]*ListItem
+	keysItems   map[Key]*ListItem // to have o(1) access
+	mutex       sync.Mutex
+}
 
-	capacity int
-	queue    List
-	items    map[Key]*ListItem
+func (lru *lruCache) Clear() {
+	lru.mutex.Lock()
+	defer lru.mutex.Unlock()
+	lru.valuesQueue = NewList()
+	lru.keysQueue = NewList()
+	lru.valuesItems = make(map[Key]*ListItem, lru.capacity)
+	lru.keysItems = make(map[Key]*ListItem, lru.capacity)
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	lru.mutex.Lock()
+	_, exist := lru.valuesItems[key]
+	var result interface{}
+	if exist {
+		item := lru.valuesItems[key]
+		result = item.Value
+		lru.valuesQueue.MoveToFront(item)
+		lru.keysQueue.MoveToFront(lru.keysItems[key])
+	}
+	defer lru.mutex.Unlock()
+	return result, exist
+}
+
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	lru.mutex.Lock()
+	defer lru.mutex.Unlock()
+	_, exist := lru.valuesItems[key]
+	if exist {
+		item := lru.valuesItems[key]
+		item.Value = value
+		lru.valuesQueue.MoveToFront(item)
+		lru.keysQueue.MoveToFront(lru.keysItems[key])
+	} else {
+		lru.valuesQueue.PushFront(value)
+		lru.keysQueue.PushFront(key)
+
+		lru.valuesItems[key] = lru.valuesQueue.Front()
+		lru.keysItems[key] = lru.keysQueue.Front()
+		if lru.valuesQueue.Len() > lru.capacity {
+			extraElementsCount := lru.valuesQueue.Len() - lru.capacity
+			for i := 0; i < extraElementsCount; i++ {
+				keyToDelete := lru.keysQueue.Back().Value.(Key)
+				delete(lru.valuesItems, keyToDelete)
+				delete(lru.keysItems, keyToDelete)
+				lru.valuesQueue.Remove(lru.valuesQueue.Back())
+				lru.keysQueue.Remove(lru.keysQueue.Back())
+			}
+		}
+	}
+	return exist
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
-		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		capacity:    capacity,
+		valuesQueue: NewList(),
+		keysQueue:   NewList(),
+		valuesItems: make(map[Key]*ListItem, capacity),
+		keysItems:   make(map[Key]*ListItem, capacity),
 	}
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -29,33 +29,75 @@ func TestCache(t *testing.T) {
 		wasInCache = c.Set("bbb", 200)
 		require.False(t, wasInCache)
 
-		val, ok := c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 100, val)
+		checkGet(t, c, "aaa", 100, true)
 
-		val, ok = c.Get("bbb")
-		require.True(t, ok)
-		require.Equal(t, 200, val)
+		checkGet(t, c, "bbb", 200, true)
 
 		wasInCache = c.Set("aaa", 300)
 		require.True(t, wasInCache)
 
-		val, ok = c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 300, val)
+		checkGet(t, c, "aaa", 300, true)
 
-		val, ok = c.Get("ccc")
-		require.False(t, ok)
-		require.Nil(t, val)
+		checkGet(t, c, "ccc", nil, false)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("clear test", func(t *testing.T) {
+		c := NewCache(4)
+		c.Set("1", 1)
+		c.Set("2", 2)
+
+		checkGet(t, c, "1", 1, true)
+		checkGet(t, c, "2", 2, true)
+		lru := c.(*lruCache)
+		require.Equal(t, lru.valuesQueue.Len(), 2)
+		require.Equal(t, len(lru.valuesItems), 2)
+
+		c.Clear()
+
+		checkGet(t, c, "1", nil, false)
+		checkGet(t, c, "2", nil, false)
+		require.Equal(t, lru.valuesQueue.Len(), 0)
+		require.Equal(t, len(lru.valuesItems), 0)
+	})
+
+	t.Run("overload test", func(t *testing.T) {
+		c := NewCache(3)
+		lru := c.(*lruCache)
+
+		c.Set("1", 1) // [1]
+		c.Set("2", 2) // [2,1]
+		c.Set("3", 3) // [3,2,1]
+		checkCapacity(t, lru, 3)
+
+		c.Set("4", 4) // [4,3,2]
+		checkCapacity(t, lru, 3)
+		checkGet(t, c, "4", 4, true) // [4,3,2]
+		checkGet(t, c, "3", 3, true) // [3,4,2]
+		checkGet(t, c, "2", 2, true) // [2,3,4]
+		checkGet(t, c, "1", nil, false)
+
+		c.Set("4", 44) // [44,2,3]
+		c.Set("3", 33) // [33,44,2]
+		c.Set("5", 5)  // [5,33,44]
+
+		checkGet(t, c, "1", nil, false)
+		checkGet(t, c, "2", nil, false)
+		checkGet(t, c, "3", 33, true)
+		checkGet(t, c, "4", 44, true)
+		checkGet(t, c, "5", 5, true)
+		checkCapacity(t, lru, 3)
+
+		c.Clear()
+
+		checkCapacity(t, lru, 0)
+		checkGet(t, c, "3", nil, false)
+		checkGet(t, c, "4", nil, false)
+		checkGet(t, c, "5", nil, false)
 	})
 }
 
-func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
+func TestCacheMultithreading(_ *testing.T) {
+	// t.Skip() // Remove me if task with asterisk completed.
 
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
@@ -76,4 +118,19 @@ func TestCacheMultithreading(t *testing.T) {
 	}()
 
 	wg.Wait()
+}
+
+func checkGet(t *testing.T, c Cache, key Key, expectedValue interface{}, exist bool) {
+	t.Helper()
+	val, ok := c.Get(key)
+	require.Equal(t, ok, exist)
+	require.Equal(t, expectedValue, val)
+}
+
+func checkCapacity(t *testing.T, lru *lruCache, length int) {
+	t.Helper()
+	require.Equal(t, lru.valuesQueue.Len(), length)
+	require.Equal(t, lru.keysQueue.Len(), length)
+	require.Equal(t, len(lru.valuesItems), length)
+	require.Equal(t, len(lru.keysItems), length)
 }
