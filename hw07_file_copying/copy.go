48c9c09c4d112e0606e28e087b5ff4392029030c
eldarkanikov <kanikov90@gmail.com>
2025-02-16 01:05:27 +0400

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,15 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeValues        = errors.New("limit and offset must be equal 0 (default) or positive")
 )
 
+const bufferSize = 1024
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if limit < 0 || offset < 0 {
+		fmt.Printf("error passing incorrect input : %v\n", ErrNegativeValues)
+		return ErrNegativeValues
+	}
+
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		fmt.Printf("error getting fileStat : %v\n", err)
+		return err
+	}
+	fileSize := fileInfo.Size()
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	sFile, err := os.Open(fromPath)
+	if err != nil {
+		fmt.Printf("error Opening file : %v\n", err)
+		return err
+	}
+	defer sFile.Close()
+	reader := bufio.NewReader(sFile)
+
+	dFile, _ := os.Create(toPath)
+	defer dFile.Close()
+	writer := bufio.NewWriter(dFile)
+
+	if limit == 0 || limit > fileSize-offset {
+		limit = fileSize - offset
+	}
+
+	bar := pb.Full.Start64(limit)
+	barWriter := bar.NewProxyWriter(writer)
+
+	if offset > 0 {
+		_, err := sFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			fmt.Printf("error seeking file : %v\n", err)
+			return err
+		}
+	}
+
+	buffer := make([]byte, bufferSize)
+
+	var totalCopied int64
+
+	for totalCopied < limit {
+		remain := limit - totalCopied
+
+		if remain < bufferSize {
+			buffer = make([]byte, remain)
+		}
+
+		read, err := reader.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error reading bytes file : %v\n", err)
+			return err
+		}
+		_, err = barWriter.Write(buffer[:read])
+		if err != nil {
+			fmt.Printf("error writing bytes file : %v\n", err)
+			return err
+		}
+		err = writer.Flush()
+		if err != nil {
+			fmt.Printf("error flushing the writer : %v\n", err)
+			return err
+		}
+		totalCopied += int64(read)
+	}
+	bar.Finish()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,72 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	inPath := "testdata/input.txt"
+	outPath := "out.txt"
+	t.Run("offset exceed", func(t *testing.T) {
+		err := Copy(inPath, outPath, 7000, 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("offset negative", func(t *testing.T) {
+		err := Copy(inPath, outPath, -1, 0)
+		require.Equal(t, err, ErrNegativeValues)
+	})
+
+	t.Run("limit negative", func(t *testing.T) {
+		err := Copy(inPath, outPath, 2, -1)
+		require.Equal(t, err, ErrNegativeValues)
+	})
+
+	t.Run("successful case, offset 100, limit 10000", func(t *testing.T) {
+		defer os.Remove(outPath)
+
+		err := Copy(inPath, outPath, 100, 1000)
+		require.NoError(t, err)
+
+		stat, _ := os.Stat(outPath)
+		require.Equal(t, int64(1000), stat.Size())
+	})
+
+	t.Run("successful case, offset 0, limit 0", func(t *testing.T) {
+		defer os.Remove(outPath)
+
+		err := Copy(inPath, outPath, 0, 0)
+		require.NoError(t, err)
+
+		sStat, err := os.Stat(inPath)
+		require.NoError(t, err)
+
+		oStat, err := os.Stat(outPath)
+		require.NoError(t, err)
+
+		require.Equal(t, sStat.Size(), oStat.Size())
+	})
+
+	t.Run("file doesn't exist", func(t *testing.T) {
+		err := Copy("testdata/non_existent.txt", outPath, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("limit is exceeded", func(t *testing.T) {
+		defer os.Remove(outPath)
+
+		err := Copy(inPath, outPath, 0, 999999)
+		require.NoError(t, err)
+
+		sStat, err := os.Stat(inPath)
+		require.NoError(t, err)
+
+		oStat, err := os.Stat(outPath)
+		require.NoError(t, err)
+
+		require.Equal(t, sStat.Size(), oStat.Size())
+	})
 }
